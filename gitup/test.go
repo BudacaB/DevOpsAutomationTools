7f99fccf8dc47610f944db90a564f8903abd2f7a
BudacaB <budaca.b@gmail.com>
2020-02-23 03:49:54 +0200

    test

---
+++ gitup/test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func execute() {
+
+	var commitMessage string
+	var branchName string
+
+	flag.StringVar(&commitMessage, "m", "", "Input commit message (Required)")
+	flag.StringVar(&branchName, "b", "", "Input branch name - master or other (Required)")
+	flag.Parse()
+
+	if commitMessage == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if branchName == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	outStatus, err := exec.Command("git", "status").Output()
+	outputStatus := string(outStatus[:])
+	fmt.Println(outputStatus)
+
+	time.Sleep(1 * time.Second)
+
+	outAdd := exec.Command("git", "add", ".")
+	err = outAdd.Run()
+
+	time.Sleep(2 * time.Second)
+
+	outCommit, err := exec.Command("git", "commit", "-am", fmt.Sprintf("%s", commitMessage)).Output()
+	outputCommit := string(outCommit[:])
+	fmt.Println(outputCommit)
+
+	time.Sleep(2 * time.Second)
+
+	var stdBuffer bytes.Buffer
+	mwriter := io.MultiWriter(os.Stdout, &stdBuffer)
+	outPush := exec.Command("git", "push", "origin", fmt.Sprintf("%s", branchName))
+	outPush.Stderr = mwriter
+	outPush.Stdout = mwriter
+	err = outPush.Run()
+	fmt.Println(stdBuffer.String())
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	} else {
+		fmt.Println("Command Successfully Executed")
+	}
+
+}
+
+func main() {
+	execute()
+}
